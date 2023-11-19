6bea514e086b9d9bd63f5fd272399a10f0297a86
tayyabaameen <151434199+tayyabaameen@users.noreply.github.com>
2023-11-19 02:03:18 -0800

    Add files via upload

---
+++ program grade.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int marks;
+	cout<<"Enter a marks : ";
+	cin>>marks;
+	if(marks>=80)
+	cout<<"grade A";
+	else if(marks>=60)
+	cout<<"grade B";
+		else if(marks>=50)
+	cout<<"grade C";
+		else if(marks>=45)
+	cout<<"grade D";
+	else
+	cout<<"fail";
+	"return0";
+}
